726c13cc6f8bb5bc7232350f5dffda45df27a657
John Parker <jparker@urgetopunt.com>
2008-06-25 18:19:14 -0700

    Added some default helpers (#title, #link_to_show, etc.) and default app layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title(text)
+    content_tag(:h1, content_for(:title) { h text })
+  end
+  
+  def link_to_show(url, options = {})
+    link_to 'show', url, options
+  end
+  
+  def link_to_edit(url, options = {})
+    link_to 'edit', url, options
+  end
+  
+  def link_to_destroy(url, options = {})
+    options.reverse_merge!(:method => :delete, :confirm => "This can't be undone. Really delete?")
+    link_to 'delete', url, options
+  end
 end
